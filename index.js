5e7231b99fca8a0ba7ed92e2c124374006ca0c92
Raz <christopher.d.rasberry@gmail.com>
2026-03-23 16:58:46 -0400

    Random Social Media

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here!
+// Display Posts function
+function displayPosts(posts) {
+    const postList = document.getElementById('post-list');
+
+    posts.forEach(function(post) {
+        const li = document.createElement('li');
+        const h1 = document.createElement('h1');
+        const p = document.createElement('p');
+
+        h1.textContent = post.title;
+        p.textContent = post.body;
+
+        li.appendChild(h1);
+        li.appendChild(p);
+
+        postList.appendChild(li);
+    });
+}
+
+// Fetch data with async/await
+async function getPosts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
+    displayPosts(posts);
+}
+
+getPosts();
